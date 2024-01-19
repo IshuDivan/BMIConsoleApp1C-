eca0bd5697d20984de35c51cdefd46cc815ff3c0
New <New@KOMPUTER>
2024-01-19 20:12:28 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1C#/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1C_
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter your weight in kilograms:");
+            double weight = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter your height in cantimeters:");
+            double height = double.Parse(Console.ReadLine());
+
+            double bmi = CalculateBMI(weight, height);
+            string bmiCategory = GetBMICategory(bmi);
+
+            Console.WriteLine($"IBM = {bmi:F2} - {bmiCategory}");
+            Console.ReadLine();
+        }
+
+        static double CalculateBMI(double weight, double height)
+        {
+            return weight / (height * height/(100*100));
+        }
+
+        static string GetBMICategory(double bmi)
+        {
+            if (bmi < 18.5)
+                return "below average";
+            else if (bmi >= 18.5 && bmi < 24.9)
+                return "Average";
+            else
+                return "Above average";
+        }
+    }
+}
