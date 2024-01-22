04437f24c23251cc31b1c0e6e1fade9ed2e911e7
Иван Дубков <d.vanya2001@gmail.com>
2024-01-23 02:23:19 +0300

    переделал массив parameters в словарь с title cтавшим ключем словаря

--- ConsoleApp1C#/Program.cs
+++ ConsoleApp1C#/Program.cs
@@ -1,27 +1,14 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml.Linq;
 
 namespace ConsoleApp1C_
 {
     internal class Program
     {
-        public class Measurement
+        public static Dictionary<string, Dictionary<string, Func<double, double>>> Parameters = new Dictionary<string, Dictionary<string, Func<double, double>>>
         {
-            public string Title { get; set; }
-            public Dictionary<string, Func<double, double>> ScalingUnits { get; set; }
-        }
-
-        public static Measurement[] Parameters =
-        {
-            new Measurement
             {
-                Title = "height",
-                ScalingUnits = new Dictionary<string, Func<double, double>>
+                "height", new Dictionary<string, Func<double, double>>
                 {
                     {"meters", value => value},
                     {"centimeters", value => value * 0.01},
@@ -29,20 +16,16 @@ namespace ConsoleApp1C_
                     {"feet", value => value * 0.3048},
                 }
             },
-            new Measurement
             {
-                Title = "weight",
-                ScalingUnits = new Dictionary<string, Func<double, double>>
+                "weight", new Dictionary<string, Func<double, double>>
                 {
                     {"kilograms", value => value},
                     {"grams", value => value * 0.001},
                     {"pounds", value => value * 0.4536},
                 }
             },
-            new Measurement
             {
-                Title = "temperature",
-                ScalingUnits = new Dictionary<string, Func<double, double>>
+                "temperature", new Dictionary<string, Func<double, double>>
                 {
                     {"Celsius", value => value},
                     {"Kelvin", value => value - 273.15},
@@ -50,25 +33,26 @@ namespace ConsoleApp1C_
                 }
             }
         };
+
         public static double belowAverageBMI = 18.5;
         public static double aboveAverageBMI = 25;
+
         static void Main()
         {
             BMICategory();
         }
+
         static void BMICategory()
         {
             double height = GetUnit("height");
             double weight = GetUnit("weight");
-            //double temperature = GetUnit("temperature");
-            //Console.WriteLine(temperature.ToString());
             double bmi = CalculateBMI(weight, height);
             string bmiCategory = GetBMICategory(bmi);
 
             Console.WriteLine($"IBM = {bmi:F3} - {bmiCategory}");
             Console.ReadLine();
-
         }
+
         static double GetUnit(string Title)
         {
             string ScalingUnits = SetScalingUnits(Title);
@@ -76,19 +60,12 @@ namespace ConsoleApp1C_
             value = NormalizeValue(value, Title, ScalingUnits);
             return value;
         }
+
         static string SetScalingUnits(string Title)
         {
-            int number = 0;
-            for (int j = 0; j < Parameters.Length; j++)
-            {
-                if (Parameters[j].Title == Title)
-                {
-                    number = j;
-                }
-            }
-            Console.WriteLine($"Enter units of {Parameters[number].Title} in which you are going to operate:");
+            Console.WriteLine($"Enter units of {Title} in which you are going to operate:");
             int i = 1;
-            foreach (KeyValuePair<string, Func<double, double>> entry in Parameters[number].ScalingUnits)
+            foreach (KeyValuePair<string, Func<double, double>> entry in Parameters[Title])
             {
                 Console.WriteLine($"To use {entry.Key} type {i}");
                 i++;
@@ -101,7 +78,7 @@ namespace ConsoleApp1C_
                 {
                     Console.WriteLine("invalid input. Couldn't find a number in your input. Try again");
                 }
-                else if (id <= 0.0 | id > Parameters[number].ScalingUnits.Count)
+                else if (id <= 0.0 | id > Parameters[Title].Count)
                 {
                     Console.WriteLine($"invalid input, only enter one of the numbers on the list. Try again");
                 }
@@ -111,7 +88,7 @@ namespace ConsoleApp1C_
                 }
             }
             i = 1;
-            foreach (KeyValuePair<string, Func<double, double>> entry in Parameters[number].ScalingUnits)
+            foreach (KeyValuePair<string, Func<double, double>> entry in Parameters[Title])
             {
                 if (i == id)
                 {
@@ -121,6 +98,7 @@ namespace ConsoleApp1C_
             }
             return null;
         }
+
         static double GetMeasurements(string name, string ScalingUnits)
         {
             string input;
@@ -143,10 +121,12 @@ namespace ConsoleApp1C_
                 }
             }
         }
+
         private static double NormalizeValue(double value, string title, string scalingUnit)
         {
-            return Parameters[Array.FindIndex(Parameters, p => p.Title == title)].ScalingUnits[scalingUnit](value);
+            return Parameters[title][scalingUnit](value);
         }
+
         static double CalculateBMI(double weight, double height)
         {
             return weight / (height * height);
@@ -162,5 +142,4 @@ namespace ConsoleApp1C_
                 return "Above average";
         }
     }
-};
-
+}
